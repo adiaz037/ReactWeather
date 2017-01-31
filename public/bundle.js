5109ce34dc6915476ba2aafa1e2e05331e9f4a53
Abraham Diaz <Abraham Diaz>
2017-01-31 11:11:30 -0500

    Update middleware for localhost

--- public/bundle.js
+++ public/bundle.js
@@ -26617,9 +26617,18 @@
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples component!!!!'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to examples page'
+	    )
 	  );
 	};
 	module.exports = Examples;

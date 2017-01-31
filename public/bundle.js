108f79c69eb6c8292d9e1ca4f3eabcacc7376c46
Abraham Diaz <Abraham Diaz>
2017-01-31 16:45:10 -0500

    add grid system and style examples

--- public/bundle.js
+++ public/bundle.js
@@ -24925,14 +24925,19 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main Component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 	module.exports = Main;
+
+	//any css classes added to main.jsx get implemented on all children components
 
 /***/ },
 /* 224 */
@@ -26733,19 +26738,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples'
 	    ),
 	    React.createElement(
 	      'p',
 	      null,
-	      'Welcome to examples page'
+	      'Here are a few example locations to try out:'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?locations=Chicago' },
+	          'Chicago'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?locations=Austin' },
+	          'Austin'
+	        )
+	      )
 	    )
 	  );
 	};
